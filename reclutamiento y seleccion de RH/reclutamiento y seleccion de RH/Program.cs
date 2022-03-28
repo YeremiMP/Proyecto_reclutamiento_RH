b3283eb304081127454f3a7038cf66bf833591c0
YeremiMP <85116554+YeremiMP@users.noreply.github.com>
2022-03-28 13:18:25 -0400

    Add files via upload

---
+++ reclutamiento y seleccion de RH/reclutamiento y seleccion de RH/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reclutamiento_y_seleccion_de_RH
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
